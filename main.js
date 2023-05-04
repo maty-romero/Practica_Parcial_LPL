35ad158fce597a2b4743b25a71499c47b7306435
Hans <ich.arbeite.rd>
2023-05-04 17:52:42 -0300

    k

---
+++ main.js
@@ -0,0 +1,18 @@
+//variables globales. 
+var aciertos, intentos, btn1, btn2;
+var pilaBtnSeleccionados = []; 
+
+function iniciarJuego{
+	var botones = document.getElementsByClassName("carta");
+	for(let i=0; i<botones.length; i++){
+		botones[i].addEventListener("click", cartaSeleccionada)
+	}	
+}
+
+function cartaSeleccionada(valorBtn){
+	var botones = document.getElementsByClassName("");
+	//busco que boton hizo click 
+	for(let i=0; i<botones.length; i++){
+		
+	}
+}
