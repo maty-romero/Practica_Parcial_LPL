2715aa43efac8a787856a64ac80265888b366bcf
maty-romero <110309525+maty-romero@users.noreply.github.com>
2023-05-05 17:30:28 -0300

    iniciar juego y Carta Seleccionada funciones

--- main.js
+++ main.js
@@ -5,14 +5,26 @@ var pilaBtnSeleccionados = [];
 function iniciarJuego{
 	var botones = document.getElementsByClassName("carta");
 	for(let i=0; i<botones.length; i++){
-		botones[i].addEventListener("click", cartaSeleccionada)
-	}	
+		botones[i].addEventListener("click", function(){
+			cartaSeleccionada(botones[i].value);
+		});
+	}
 }
 
 function cartaSeleccionada(valorBtn){
 	var botones = document.getElementsByClassName("");
 	//busco que boton hizo click 
 	for(let i=0; i<botones.length; i++){
-		
+		if(botones[i].value == valorBtn){
+			pilaBtnSeleccionados.push(botones[i]);
+			break; 
+		}	
+	}
+	deshabilitarCarta(pilaBtnSeleccionados.peek()) //deshabilito la ultima carta 
+	//si hay 2 botones seleccionados -> evaluo
+	if(pilaBtnSeleccionados.length == 1){
+		evaluarTirada();
 	}
 }
+
+
