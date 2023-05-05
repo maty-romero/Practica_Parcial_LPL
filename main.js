cd6fec7033faa31445c5b60c7d0288fbc7ac2ee3
Maximiliano Vera <108048616+Prohibidou@users.noreply.github.com>
2023-05-05 19:17:12 -0300

    Cambios en iniciarJuego, finalizaJuego (apretar el boton finalizarJuego)

--- main.js
+++ main.js
@@ -2,33 +2,53 @@
 var aciertos, intentos, btn1, btn2;
 var pilaBtnSeleccionados = []; 
 
-function iniciarJuego(){
+	function iniciarJuego(){
 	var botones = document.getElementsByClassName("carta");
 	for(let i=0; i<botones.length; i++){
 		botones[i].addEventListener("click", function(){
 			cartaSeleccionada(botones[i]);
 		});
 	}
+	//habilito los botones pues si el usuario apretó antes el boton FinalizarJuego(), me quedarían todos deshabilitados al apretar iniciarJuego
+	habilitarBotones();
+	
 }
 
 
-function cartaSeleccionada(boton){
-	//var botones = document.getElementsByClassName("carta");
-	
+	function cartaSeleccionada(boton){
 	pilaBtnSeleccionados.push(boton);	
-	
-	console.log(pilaBtnSeleccionados.length);
 	deshabilitarCarta(boton);
-	//deshabilitarCarta(pilaBtnSeleccionados[pilaBtnSeleccionados.length - 1]) //deshabilito la ultima carta 
 	//si hay 2 botones seleccionados -> evaluo
 	if(pilaBtnSeleccionados.length == 2){
 		evaluarTirada();
 	}
 }
 
-function deshabilitarCarta(boton){
+	function deshabilitarCarta(boton){
 	boton.disabled = true; 
 }
+
+
+//habilito los botones
+	function habilitarBotones() {
+	var botones = document.getElementsByClassName("carta");
+	for(let i=0; i<botones.length; i++){
+		botones[i].disabled=false;	
+	}
+	
+	}
+
+	function finalizaJuego() {
+	var botones = document.getElementsByClassName("carta");
+		for(let i=0; i<botones.length; i++){
+		botones[i].disabled=true;
+	}
+		var divLeyenda= document.getElementById("leyenda");
+    var pFinalizarJuego = document.createElement("p");
+	pFinalizarJuego.innerHTML="FIN DEL JUEGO"
+	pFinalizarJuego.id="idPfinalizarJuego"
+	divLeyenda.appendChild(pFinalizarJuego);
+	}
 
 
 
